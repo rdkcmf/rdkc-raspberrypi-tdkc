703e0378ce4e6160c37dbe0f4a493d14721991a9
svelus003c <sugasini_velusamy@comcast.com>
2020-02-21 16:16:54 +0000

    TDK-2334 : Porting of TDK to RPI RDK-C Stack
    Reason for change: Add TDK-C platform specific scripts
    Test Procedure:Check whether tdk-c online in TM
    Risks: None
    Signed-off-by: svelusamy <svelusamy@tataelxsi.co.in>
    Change-Id: If5850392ca0afbbfd31901a244f9566543a98835

---
+++ agent/scripts/StartTDK.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+#Setting up environment to run TDK
+export TDK_PATH=/opt/TDK/
+export TDK_BIN_PATH=/usr/bin/
+export TDK_LOGGER_PATH=/opt/TDK/logs/
+export TDK_PROPERTIES_PATH=/etc/
+export PATH=$PATH:/usr/local/bin:$TDK_BIN_PATH
+export TDK_LIB_PATH=$TDK_PATH/libs/
+export LD_LIBRARY_PATH=$TDK_LOGGER_PATH/libs/:$TDK_PATH/libs/:/usr/local/lib/:/usr/local/Qt/lib/:/mnt/nfs/lib:/mnt/nfs/bin/target-snmp/lib/:/mnt/nfs/bin:/usr/local/lib/sa:$LD_LIBRARY_PATH
+
+export PATH HOME LD_LIBRARY_PATH
+ulimit -c unlimited
+
+echo "Going to start Agent"
+cd $TDK_BIN_PATH/
+./rdk_tdk_agent_process &
+sleep 5
+status=`ps | grep tdk_agent | grep -v grep`
+if [  "$status" ];
+then
+    echo "Starting Agent Monitor Process.."
+    sleep 5
+    cd $TDK_PATH/
+    sh TDKagentMonitor.sh & 
+fi
+
+
